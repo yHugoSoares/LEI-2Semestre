29989f096297333439b88a8514b1f3367a32e03d
Hugo Soares <a107293@alunos.uminho.pt>
2024-02-19 20:24:06 +0000

    Changes to LI2

--- LI/LI2.c
+++ LI/LI2.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
 
-void converter_para_yijing(unsigned long long int num) 
+void converter_para_yijing(long num) 
 {
-    char yijing[64] =   "䷀	䷁ 	䷂ 	䷃ 	䷄ 	䷅ 	䷆ 	䷇ 	䷈ 	䷉ 	䷊ 	䷋ 	䷌ 	䷍ 	䷎ 	䷏"
-                        "䷐ ䷑ 	䷒ 	䷓ 	䷔ 	䷕ 	䷖ 	䷗ 	䷘ 	䷙ 	䷚ 	䷛ 	䷜ 	䷝ 	䷞ 	䷟"
-                        "䷠ ䷡ 	䷢ 	䷣ 	䷤ 	䷥ 	䷦ 	䷧ 	䷨ 	䷩ 	䷪ 	䷫ 	䷬ 	䷭ 	䷮ 	䷯"
-                        "䷰ ䷱ 	䷲ 	䷳ 	䷴ 	䷵ 	䷶ 	䷷ 	䷸ 	䷹ 	䷺ 	䷻ 	䷼ 	䷽ 	䷾ 	䷿";
+    // Yijing
+    char *yijing[64] = 
+    {
+        "䷀", "䷁", "䷂", "䷃", "䷄", "䷅", "䷆", "䷇", "䷈", "䷉", "䷊", "䷋", "䷌", "䷍", "䷎", "䷏",
+        "䷐", "䷑", "䷒", "䷓", "䷔", "䷕", "䷖", "䷗", "䷘", "䷙", "䷚", "䷛", "䷜", "䷝", "䷞", "䷟",
+        "䷠", "䷡", "䷢", "䷣", "䷤", "䷥", "䷦", "䷧", "䷨", "䷩", "䷪", "䷫", "䷬", "䷭", "䷮", "䷯",
+        "䷰", "䷱", "䷲", "䷳", "䷴", "䷵", "䷶", "䷷", "䷸", "䷹", "䷺", "䷻", "䷼", "䷽", "䷾", "䷿"
+    };
+
     char resultado[64];
     int i = 0;
 
+    // Converte o número para base 64
     while (num > 0) 
     {
-        resultado[i++] = yijing[num % 64];
+        resultado[i++] = *yijing[num % 64];
         num /= 64;
     }
 
+    // Imprime os caracteres correspondentes ao número convertido
     for (int j = i - 1; j >= 0; j--) 
     {
-        printf("%c ", resultado[j]);
+        printf("%s ", &resultado[j]);
     }
 }
 
 int main() 
 {
-    unsigned long long int num;
-    printf("Digite um número: ");
-    scanf("%llu", &num);
+    long num;
+    //printf("Digite um número: ");
+    scanf("%ld", &num);
 
-    printf("O número %llu convertido para o sistema Yijing é: \n", num);
+    printf("O número %ld convertido para o sistema Yijing é: \n", num);
     converter_para_yijing(num);
 
     return 0;
