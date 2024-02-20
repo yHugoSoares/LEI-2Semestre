a191211dc31e1367521af5b5e0ffa62d354f748e
Hugo Soares <a107293@alunos.uminho.pt>
2024-02-20 11:08:13 +0000

    LI2.2.c Finished

--- LI/LI2.c
+++ LI/LI2.c
@@ -1,41 +1,51 @@
 #include <stdio.h>
 
-void converter_para_yijing(long num) 
+void yijing(unsigned long long int x)
 {
-    // Yijing
-    char *yijing[64] = 
+    const char *unicodeChars[] = 
     {
-        "䷀", "䷁", "䷂", "䷃", "䷄", "䷅", "䷆", "䷇", "䷈", "䷉", "䷊", "䷋", "䷌", "䷍", "䷎", "䷏",
-        "䷐", "䷑", "䷒", "䷓", "䷔", "䷕", "䷖", "䷗", "䷘", "䷙", "䷚", "䷛", "䷜", "䷝", "䷞", "䷟",
-        "䷠", "䷡", "䷢", "䷣", "䷤", "䷥", "䷦", "䷧", "䷨", "䷩", "䷪", "䷫", "䷬", "䷭", "䷮", "䷯",
-        "䷰", "䷱", "䷲", "䷳", "䷴", "䷵", "䷶", "䷷", "䷸", "䷹", "䷺", "䷻", "䷼", "䷽", "䷾", "䷿"
+        "\u4dc0", "\u4dc1", "\u4dc2", "\u4dc3", "\u4dc4", "\u4dc5", "\u4dc6", "\u4dc7",
+        "\u4dc8", "\u4dc9", "\u4dca", "\u4dcb", "\u4dcc", "\u4dcd", "\u4dce", "\u4dcf",
+        "\u4dd0", "\u4dd1", "\u4dd2", "\u4dd3", "\u4dd4", "\u4dd5", "\u4dd6", "\u4dd7",
+        "\u4dd8", "\u4dd9", "\u4dda", "\u4ddb", "\u4ddc", "\u4ddd", "\u4dde", "\u4ddf",
+        "\u4de0", "\u4de1", "\u4de2", "\u4de3", "\u4de4", "\u4de5", "\u4de6", "\u4de7",
+        "\u4de8", "\u4de9", "\u4dea", "\u4deb", "\u4dec", "\u4ded", "\u4dee", "\u4def",
+        "\u4df0", "\u4df1", "\u4df2", "\u4df3", "\u4df4", "\u4df5", "\u4df6", "\u4df7",
+        "\u4df8", "\u4df9", "\u4dfa", "\u4dfb", "\u4dfc", "\u4dfd", "\u4dfe", "\u4dff"
     };
-
-    char resultado[64];
-    int i = 0;
-
-    // Converte o número para base 64
-    while (num > 0) 
+    unsigned long long int i, j = x;
+    int a[100];
+    int count = 0;
+    while (j >= 64)
     {
-        resultado[i++] = *yijing[num % 64];
-        num /= 64;
+        i = j%64;
+        j = j/64;
+        a[count] = i;
+        count++;
     }
-
-    // Imprime os caracteres correspondentes ao número convertido
-    for (int j = i - 1; j >= 0; j--) 
+    i = j%64;
+    a[count] = i;
+    while (count > 0)
     {
-        printf("%s ", &resultado[j]);
+        int t = a[count];
+        printf ("%s ", unicodeChars[t]);
+        count--;
     }
+    int t = a[count];
+    printf ("%s", unicodeChars[t]);
+    printf ("\n");
 }
 
-int main() 
+
+
+int main () 
 {
-    long num;
-    //printf("Digite um número: ");
-    scanf("%ld", &num);
-
-    printf("O número %ld convertido para o sistema Yijing é: \n", num);
-    converter_para_yijing(num);
-
+    unsigned long long int x;
+    if (scanf("%llu", &x) != 1) 
+    {
+        fprintf(stderr, "Erro ao ler o dia.\n");
+        return 1;
+    }
+    yijing (x);
     return 0;
 }
