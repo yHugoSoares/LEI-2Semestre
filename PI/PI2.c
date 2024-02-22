0355854b703d32bdb1d8068b38446b3e012e7383
Hugo Soares <a107293@alunos.uminho.pt>
2024-02-22 19:27:05 +0000

    Added some functions to PI2

--- PI/PI2.c
+++ PI/PI2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-void multiplicacao(float m, int n) 
+int multiplicacao(float m, int n) 
 {
     float r = 0;
     scanf("%f %i",&m, &n);
@@ -12,7 +12,7 @@ void multiplicacao(float m, int n)
     printf("O resultado da multiplicação %.2f*%i = %.2f\n", m, n, r);
 }
 
-void multotimizada(int  p, int o)
+int multotimizada(int  p, int o)
 {
     scanf("%i %i", &o, &p);
     int t = 0;
@@ -59,10 +59,74 @@ int mdc2(int a, int b)
     }
 }
 
+int mdc3(int c, int d)
+{
+    printf("Escolha dois numeros:\n");
+    scanf("%i %i", &c, &d);
+    while (c != d)
+    {
+        if (c > d)
+        {
+            c = c % d;
+        }
+        else
+        {
+            d = d % c;
+        }
+    }
+    if (c == d)
+    {
+        printf("O mdc é: %i\n", c);
+    }
+}
+int fib1 (int e)
+{
+    if (e < 2)
+    {
+        return e;
+    }
+    else
+    {
+        return fib1(e - 1) + fib1(e - 2);
+    }
+}
+
+int fib2(int n) 
+{
+    int fib_atual = 0;
+    int fib_anterior = 1;
+    int fib_anterior_anterior = 0;
+
+    if (n < 2) 
+    {
+        return n;
+    }
+
+    for (int i = 2; i <= n; i++) 
+    {
+        fib_atual = fib_anterior + fib_anterior_anterior;
+        fib_anterior_anterior = fib_anterior;
+        fib_anterior = fib_atual;
+    }
+
+    return fib_atual;
+}
+
 int main()
 {
-    int m, n, o, p, a, b;
-    mdc2(a, b);
+    // Defenicao Variaveis
+    int m, n, o, p, a, b, c, d, e, f;
+    
+    // scanf("%i", &e);
+    // fib1(e);
+    // printf("Finobacci desse número é: %i\n", fib1(e));
+    scanf("%i", &f);
+    fib2(f);
+    printf("Finobacci desse número é: %i\n", fib2(f));
+
+    //mdc2(a, b);
+    //mdc3(c, d);
     //multiplicacao(m, n);
     //multotimizada(o, p);
+    
 }
