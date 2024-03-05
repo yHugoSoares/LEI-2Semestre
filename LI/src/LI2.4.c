87ea060813761a94691bbf23494a80bc4f42dedf
Hugo Soares <a107293@alunos.uminho.pt>
2024-03-05 09:19:46 +0000

    Small Changes

--- LI/src/LI2.4.c
+++ LI/src/LI2.4.c
@@ -3,3 +3,27 @@
 #include <string.h>
 #include <assert.h>
 
+
+
+
+int main()
+{
+    int diferenca = 5;
+    char frase[10000];
+    char palavra[10000];
+    fgets(frase, 10000, stdin);
+    for (int i = 0; i < 10000; i++)
+    {
+        if (frase[i] == ' ')
+        {
+            palavra[i] = ' ';
+        }
+        else
+        {
+            palavra[i] = frase[i] + diferenca;
+        }
+        
+    }
+    
+    printf("%d %s\n", diferenca,  palavra);
+}
