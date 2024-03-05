95f4f9220a9d137a34d857258f62a498f9a45eaa
Hugo Soares <a107293@alunos.uminho.pt>
2024-03-05 14:32:27 +0000

    LI2.4 Base idea Done

--- LI/src/LI2.4.c
+++ LI/src/LI2.4.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-
-
-
 
 int main()
 {
@@ -14,16 +12,25 @@ int main()
     fgets(frase, 10000, stdin);
     for (int i = 0; i < 10000; i++)
     {
-        if (frase[i] == ' ')
+        if ((frase[i] > 66 && frase[i] < 91) || (frase[i] > 96 && frase[i] < 123))
         {
-            palavra[i] = ' ';
+            if ((frase[i] + diferenca > 90) && (frase[i] < 96))
+            {
+                palavra[i] = frase[i] + diferenca - 26;
+            }
+            else if (frase[i] + diferenca > 122)
+            {
+                palavra[i] = frase[i] + diferenca - 26;
+            }
+            
+            else
+            palavra[i] = frase[i] + diferenca;
         }
         else
         {
-            palavra[i] = frase[i] + diferenca;
+            palavra[i] = frase[i];
         }
-        
     }
-    
-    printf("%d %s\n", diferenca,  palavra);
+
+    printf("%d %s\n", diferenca, palavra);
 }
