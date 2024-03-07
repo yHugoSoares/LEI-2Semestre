51a6cd2fa3b7e69fb0cbd10fe6bc4403488dbf94
Hugo Soares <a107293@alunos.uminho.pt>
2024-03-07 15:46:47 +0000

    Changes

--- LI/src/LI2.4.c
+++ LI/src/LI2.4.c
@@ -27,7 +27,7 @@ size_t count_tokens(const char *frase, char token)
 // FORMULA -- SUM((Original - Obtida)^2 / Original)
 int Partial(char *frase)
 {
-    int delta = 5;
+    int delta = 0;
     char fraseUpper[10000];
     float expected[26] = {43.31, 10.56, 23.13, 17.25, 56.88, 9.24, 12.59, 15.31,
                           38.45, 1.00, 5.61, 27.98, 15.36, 33.92, 36.51, 16.14, 1.00,
@@ -39,18 +39,18 @@ int Partial(char *frase)
     for (int j = 0; j <= 26; j++)
     {
         float sum2 = 0;
-        for (long long unsigned int i = 0; i < sizeof(fraseUpper); i++)
+        for (long long unsigned int i = 0; i < strlen(fraseUpper); i++)
         {
             int times = 0;
-            int index = fraseUpper[i];
-            times = count_tokens(fraseUpper, index);
-            valueG = expected[index - 65] - j;
+            int index = fraseUpper[i] - 'A';
+            times = count_tokens(fraseUpper, fraseUpper[i]);
+            valueG = expected[index];
             sum2 += (pow((valueG - times), 2)) / valueG;
         }   
         if (sum2 < sum1)
         {
             delta = j;
-            sum2 = sum1;
+            sum1 = sum2;
         }
     }  
     return delta;
@@ -60,7 +60,10 @@ int main()
 {
     char frase[10000];
     char palavra[10000];
-    fgets(frase, 10000, stdin);
+    if (fgets(frase, 10000, stdin) == NULL)
+    {
+        return 1;
+    }
     int delta = Partial(frase);
     for (int i = 0; i < 10000; i++)
     {
