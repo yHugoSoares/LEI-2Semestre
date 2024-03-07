2d708257aca71fd98e135e07e04cc73c328350d2
Hugo Soares <a107293@alunos.uminho.pt>
2024-03-07 10:02:11 +0000

    Small changes to LI2.4

--- LI/src/LI2.4.c
+++ LI/src/LI2.4.c
@@ -3,34 +3,63 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <ctype.h>
+
+void PhraseToUpper(char *frase)
+{
+    for (int i = 0; frase[i]; i++)
+    {
+        frase[i] = toupper(frase[i]);
+    }
+}
+
+// FORMULA -- SUM((Original - Obtida)^2 / Original)
+int Partial(char *frase)
+{
+    int delta = 5;
+    char fraseUpper[10000];
+    strcpy(fraseUpper, frase);
+    PhraseToUpper(fraseUpper);
+    float expected[26] = {43.31, 10.56, 23.13, 17.25, 56.88, 9.24, 12.59, 15.31,
+                          38.45, 1.00, 5.61, 27.98, 15.36, 33.92, 36.51, 16.14, 1.00,
+                          38.64, 29.23, 35.43, 18.51, 5.13, 6.57, 1.48, 9.06, 1.39};
+    for (int i = 0; i <= 26; i++)
+    {
+        int sum = 0;
+        for (long long unsigned int j = 0; j < sizeof(fraseUpper); j++)
+        {
+            int index = fraseUpper[j] - 'A';
+            sum += expected[index - 65];   
+        }
+    }
+    
+    return delta;
+}
 
 int main()
 {
-    int diferenca = 5;
     char frase[10000];
     char palavra[10000];
     fgets(frase, 10000, stdin);
+    int delta = Partial(frase);
     for (int i = 0; i < 10000; i++)
     {
-        if ((frase[i] > 66 && frase[i] < 91) || (frase[i] > 96 && frase[i] < 123))
+        if ((frase[i] >= 'A' && frase[i] <= 'Z') || (frase[i] >= 'a' && frase[i] <= 'z'))
         {
-            if ((frase[i] + diferenca > 90) && (frase[i] < 96))
+            if ((frase[i] + delta > 'Z' && frase[i] <= 'Z') || (frase[i] + delta > 'z'))
             {
-                palavra[i] = frase[i] + diferenca - 26;
+                palavra[i] = frase[i] + delta - 26;
             }
-            else if (frase[i] + diferenca > 122)
-            {
-                palavra[i] = frase[i] + diferenca - 26;
-            }
-            
             else
-            palavra[i] = frase[i] + diferenca;
+            {
+                palavra[i] = frase[i] + delta;
+            }
         }
         else
         {
             palavra[i] = frase[i];
         }
     }
-
-    printf("%d %s\n", diferenca, palavra);
+    printf("%d %s\n", delta, palavra);
+    return 0;
 }
