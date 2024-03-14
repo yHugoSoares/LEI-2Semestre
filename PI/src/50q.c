4ca7c40144e2aad96e7aad0f2e4b08f2d74321e6
Hugo Soares <a107293@alunos.uminho.pt>
2024-03-14 19:52:29 +0000

    50q Updated

--- PI/src/50q.c
+++ PI/src/50q.c
@@ -2,42 +2,80 @@
 #include <string.h>
 #include <math.h>
 
-
-void q1()
+void q1() 
 {
-    int v[100], n = 0, count = 0;
-    int resultado = v[0];
+    int n = 0, resultado = 0;
     printf("Insira numeros inteiros (0 para terminar)\n");
-    while (1)
+    while (scanf("%d", &n) && n != 0)
     {
-        assert(scanf("%d", &n) == 1);
-        if (n == 0)
+        if (n > resultado) 
         {
-            break;
-        }
-        else count += 1;
-        for (int i = 0; i < count; i++)
-        {
-            if (resultado < v[i])
-            {
-                resultado = v[i];
-            }
+            resultado = n;
         }
     }
     printf("Maior: %d\n", resultado);
 }
 
+
 void q2() 
 {
-    double soma = 0;
-    int num, n = 0;
-    while(1) 
+    int n = 0, counter = 0;
+    float soma = 0, media = 0;
+    printf("Insira numeros para media (0 para terminar)\n");
+    while(scanf("%i", &n) && n != 0)
     {
-        assert(scanf("%d",&num) == 1);
-        if(num == 0) break;
-        soma += num;
-        n++;
+        soma += n;
+        counter++;
     }
-    double media = soma / n;
-    printf("Média: %.5f\n", media);
+    media = soma / counter;
+    printf("Media: %.2f\n", media);
+}
+
+void q3()
+{
+    int n = 0, counter = 0, v[100], max = 0, max2 = 0;
+    printf("Insira numeros inteiros (0 para terminar)\n");
+    while(scanf("%i", &n) && n != 0)
+    {
+        v[counter++] = n;
+    }
+    for (int i = 0; i < counter; i++)
+    {
+        if (max < v[i])
+        {
+            max = v[i];
+        }
+    }
+    for (int i = 0; i < counter; i++)
+    {
+        if (max2 < v[i] && v[i] != max)
+        {
+            max2 = v[i];
+        }
+    }
+    printf("2º maior: %i\n", max2);
+}
+
+int bitsUm (unsigned int n)
+{
+    int counter = 0;
+    while (n != 0)
+    {
+        if (n % 2 == 1)
+        {
+            counter++;
+            n = n / 2;
+        }
+        n = n / 2;
+    }
+    return counter;
+}
+
+int main()
+{
+    int n = 0;
+    scanf("%i", &n);
+    printf("Numero de bits 1: %i\n", bitsUm(n));
+
+    return 0;
 }
