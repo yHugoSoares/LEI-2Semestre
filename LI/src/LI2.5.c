3852195deec924874b06b3ed084558a4d73f8fa8
Hugo Soares <145548037+yHugoSoares@users.noreply.github.com>
2024-03-15 19:13:36 +0000

    Update LI2.5.c

--- LI/src/LI2.5.c
+++ LI/src/LI2.5.c
@@ -1,4 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#define TABLE_SIZE 2024
+
+
+unsigned long hash1_function(char* str)
+{
+    unsigned long i = 0;
+
+    for (int j = 0; str[j]; j++)
+        i += str[j];
+
+    return i % TABLE_SIZE;
+}
+
 int main()
 {
-    
+    char str[100], type[10];
+    scanf("%i", TABLE_SIZE);
+    fgets(type, 10, stdin);
 }
