f853c2167a8289956dd331f5e29c02b2a862750f
Hugo Soares <Hugoferreirasoares@gmail.com>
2024-03-16 15:23:04 +0000

    Small Changes to LI2.5

--- LI/src/LI2.5.c
+++ LI/src/LI2.5.c
@@ -3,22 +3,75 @@
 #include <string.h>
 #include <assert.h>
 
-#define TABLE_SIZE 2024
+#define VALUE_SIZE INT_MAX
+#define TABLE_SIZE 10000
+#define ACTUAL_TABLE *p
 
-
-unsigned long hash1_function(char* str)
+typedef struct
 {
-    unsigned long i = 0;
+    int value;
+} valor;
 
-    for (int j = 0; str[j]; j++)
-        i += str[j];
+valor * hash_table[TABLE_SIZE];
 
-    return i % TABLE_SIZE;
+unsigned long hash1_function(int value, int ACTUAL_TABLE)
+{
+    return value % ACTUAL_TABLE;
+}
+
+
+unsigned long hash2_function(int value, int ACTUAL_TABLE)
+{
+    return ((value/TABLE_SIZE) % ACTUAL_TABLE);
+}
+
+void init_hashtables()
+{
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        hash_table[i] = NULL;
+    }
+    // Initialize hashTable to NULL
+}
+
+void print_table()
+{
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        if (hash_table[i] == NULL)
+        {
+            printf("\t%i\t---\n", i);
+        }
+        else printf("%i\n", hash_table[i]->value);
+    }
+}
+
+bool hash_table_insert(valor *p)
+{
+    if (p == NULL) 
+    {
+        return false;
+    }
+    int index = hash1_function(p -> value);
+    if (hash_table[index] != NULL)
+    {
+        return false;
+    }
+    hash_table[index] = p;
+    return true;
 }
 
 int main()
 {
-    char str[100], type[10];
-    scanf("%i", TABLE_SIZE);
-    fgets(type, 10, stdin);
+    char type[10], activity;
+    int value = 0;
+    
+    init_hashtables();
+    scanf("%i", &p);
+    scanf("%i", &value);
+    print_table();
+    // fgets(type, 10, stdin);
+    // scanf("%c", &activity);
+    
+    // printf("%li\n", hash2_function(value, p));
 }
