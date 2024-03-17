221ed2750dddf83faa9c8d7897e61a52e92e203f
Hugo Soares <a107293@alunos.uminho.pt>
2024-03-17 21:17:10 +0000

    remove bin dir

--- LI/src/LI2.5.c
+++ LI/src/LI2.5.c
@@ -1,3 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define CAPACITY 50000 // Size of the HashTable.
+
+unsigned long hash_function(char* str)
+{
+    unsigned long i = 0;
+
+    for (int j = 0; str[j]; j++)
+        i += str[j];
+
+    return i % CAPACITY;
+}
+
+
+
+
 int main()
 {
     
