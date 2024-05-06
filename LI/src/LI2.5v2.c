6aaf56a9a2cb1b69196b05d712479a2ea1490253
Hugo Soares <a107293@alunos.uminho.pt>
2024-05-06 23:55:10 +0100

    .

--- LI/src/LI2.5v2.c
+++ LI/src/LI2.5v2.c
@@ -18,7 +18,195 @@ unsigned int hash_function1(int value, int slots)
 
 unsigned int hash_function2(int *value, int *slots)
 {
-    return (*value / *slots) % *slots;
+    return (floor(*value / *slots)) % *slots;
+}
+
+
+void initTable(int *open[], int slots) // init table empty (NULL)
+{
+    for (int i = 0; i < slots; i++)
+    {
+        open[i] = NULL;
+    }
+}
+
+void initDeleted(int deleted[], int slots)
+{
+    for (int i = 0; i < slots; i++)
+    {
+        deleted[i] = 0;
+    }
+}
+
+int openInsert(int *open[], int deleted[], int slots, int x)
+{
+    int key = hash_function1(x, slots);
+    int start = key;
+    int deletedSlotKey = -1;
+
+    if (open[key] == NULL)
+    {
+        open[key] = malloc(sizeof(int));
+        *open[key] = x;
+        printf("%d -> %d\nOK\n", key, *open[key]);
+        return 0;
+    }
+
+    while (open[key] != NULL)
+    {
+        if (deleted[key] == 1 && deletedSlotKey == -1)
+        {
+            deletedSlotKey = key;
+        }
+        if (*open[key] == x && deleted[key] == 0)
+        {
+            printf("%d EXISTS\n", x);
+            if (deletedSlotKey != -1)
+            {
+                *open[deletedSlotKey] = x;
+                deleted[deletedSlotKey] = 0;
+                deleted[key] = 1;
+            }
+            return 0;
+        }
+
+        key++;
+        if (key == start)
+        {
+            if (deletedSlotKey != -1) break;
+            else
+            {
+            printf("GIVING UP!\n");
+            return 1;
+            }
+        }
+        if (key == slots)
+        {
+            key = 0;
+        }
+    }
+
+    if (deletedSlotKey != -1)
+    {
+        deleted[deletedSlotKey] = 0;
+        *open[deletedSlotKey] = x;
+        printf("%d -> %d\nOK\n", deletedSlotKey, x);
+    }
+    else
+    {
+        open[key] = malloc(sizeof(int));
+        *open[key] = x;
+        printf("%d -> %d\nOK\n", key, x);
+    }
+
+    return 0;
+}
+
+void openSearch(int *open[], int deleted[], int slots, int x)
+{
+    int key = hash(0, x, slots);
+    int start = key;
+    int deletedSlotKey = -1;
+
+    while (open[key] != NULL)
+    {
+        if (deleted[key] == 1 && deletedSlotKey == -1)
+            deletedSlotKey = key;
+
+        if (*open[key] == x && deleted[key] == 0)
+        {
+            if (deletedSlotKey != -1)
+            {
+                *open[deletedSlotKey] = x;
+                deleted[deletedSlotKey] = 0;
+                deleted[key] = 1;
+                printf("%d\n", deletedSlotKey);
+            }
+            else
+                printf("%d\n", key);
+            return;
+        }
+
+        key++;
+        if (key == slots)
+            key = 0;
+
+        if (key == start)
+            break;
+    }
+
+    printf("NO\n");
+}
+
+void openFunction(int slots)
+{
+    int *open[slots];
+    int deleted[slots];
+    initTable(open, slots);
+    initDeleted(deleted, slots);
+
+    char instruction[] = "A";
+    int number = -1;
+
+    while(scanf("%s", instruction) != EOF && instruction[0] != 'S')
+    {
+        int key;
+        if (instruction[0] != 'P') assert(scanf("%d", &number) == 1);
+            
+        key = hash(0, number, slots);
+
+        if (instruction[0] == 'I')
+        {
+            if (openInsertion(open, deleted, slots, number)) break;
+        }
+        if (instruction[0] == 'C') 
+        {
+            openSearch(open, deleted, slots, number);
+        }
+        if (instruction[0] == 'D')
+        {
+            if (open[key] == NULL) printf("NO\n");
+            else if (*open[key] == number && deleted[key] == 0)
+            {
+                deleted[key] = 1;
+                printf("OK\n");
+            }
+            else openDelete(open, deleted, slots, key, number);
+        }
+
+        if (instruction[0] == 'P') 
+        {
+            openPrint(open, deleted, slots);
+        }
+    }
+}
+/* --------------------------------------------------------------- */
+
+int main()
+{
+    int slots = 0;
+    char type[] = "A";
+    
+    if(scanf("%d", &slots) != 1) return -1;
+
+    if (scanf("%s", type) != 1) return -1;
+    
+    // valor *hash_table1[slots];
+    // valor *hash_table2[slots];
+
+    if (!strcmp(type, "OPEN"))
+    {
+        openFunction(slots);
+    }
+    else if (!strcmp(type, "CUCKOO"))
+    {
+        
+    }
+    else if (!strcmp(type, "LINK"))
+    {
+        
+    }
+    return 0;
 }
 
 /*
@@ -79,148 +267,5 @@ void delete_hash_table(int *value, valor *hash_table1[], int slots)
     {
         hash_table1[index] = NULL;
     }
-}
-*/
-
-void init_open_table(int *open[], int slots) // init table empty (NULL)
-{
-    for (int i = 0; i < slots; i++)
-    {
-        open[i] = NULL;
-    }
-}
-
-
-int existsOpen(int *open[], int x, int slots)
-{
-    for (int i = 0; i < slots; i++)
-    {
-        if (x == *open[i])
-        {
-            return 1;
-        }
-    }
-    return 0;
-}
-
-void openFunction(int slots)
-{
-    char instruction[] = "A";
-    int number = -1, key = -1, *open[slots];
-    init_open_table(open, slots);
-    while (scanf("%s", instruction) != EOF || instruction[0] != 'S')
-    {
-        printf("SLOTS: %d\n", slots);
-        if (instruction[0] == 'P')
-        {
-            for (int i = 0; i < slots; i++)
-            {
-                if (open[i] != NULL)
-                {
-                    printf("%d\t%d\n", i, *open[i]);
-                }
-                
-            }
-            
-        }
-        else if (instruction[0] == 'I')
-        {
-            printf("SLOTS: %d\n", slots);
-            assert(scanf("%d", &number) == 1);
-            printf("SLOTS: %d\n", slots);    
-            if (existsOpen(open, number, slots)) printf("%i EXISTS\n", number);
-            else
-            {
-                key = hash_function1(number, slots);
-                *open[key] = number;
-                printf("%d -> %d\nOK\n", key, number);
-            }
-        }
-    }
-}
-
-int main()
-{
-    int slots = 0;
-    char type[] = "A";
-    
-    if(scanf("%d", &slots) != 1) return -1;
-
-    if (scanf("%s", type) != 1) return -1;
-    
-    // valor *hash_table1[slots];
-    // valor *hash_table2[slots];
-
-    if (!strcmp(type, "OPEN"))
-    {
-        openFunction(slots);
-    }
-    else if (!strcmp(type, "CUCKOO"))
-    {
-        
-    }
-    else if (!strcmp(type, "LINK"))
-    {
-        
-    }
-    return 0;
-}
-
-/*
-int main()
-{
-    char instr;
-    int value = 0, size_table = 0;
-    if (scanf("%i", &size_table) != 1) return -1;
-    if (scanf("%c", &instr) != 1) return -1;
-    while (getchar() != '\n')
-    {
-        if (scanf("%c", &instr) != 1) return -1;
-        if (scanf("%i", &value) != 1) return -1;
-        if (instr == 'I')
-        {
-            if (insert_hash_table(&value)) printf(" -> %i\n", value);
-            else if (insert_hash_table(&value) == 2) printf("%i EXISTS", value);
-            else printf("%i ERROR", value);
-        }
-
-        if (instr == 'D')
-        {
-            delete_hash_table(&value);
-        }
-
-        if (instr == 'C')
-        {
-            
-        }
-
-        if (instr == 'P')
-        {
-            print_hash_table();
-        }
-    }
-    
-    
-    init_hash_table();
-    valor a = {.value = 5};
-    valor b = {.value = 142};
-    valor c = {.value = 231};
-    valor d = {.value = 476};
-    valor e = {.value = 495};
-    valor f = {.value = 347};
-    
-    insert_hash_table(&a);
-    insert_hash_table(&b);
-    insert_hash_table(&c);
-    insert_hash_table(&d);
-    insert_hash_table(&e);
-    insert_hash_table(&f);
-
-
-    print_hash_table();
-
-    
-
-    print_hash_table();
 }
 */
---
+++ LI/src/problemaE.c
@@ -0,0 +1,528 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <locale.h>
+#include <wchar.h>
+#include <assert.h>
+
+int hash(int table, int number, int tableSize)
+{
+    if (table == 0)
+        return number % tableSize;
+    else
+    {
+        int n = floor(number / tableSize);
+        return n % tableSize;
+    }
+}
+
+void initTable(int * table[], int tableSize)
+{
+    for(int i = 0; i < tableSize; i++)
+        table[i] = NULL;
+}
+
+void initDeleted(int deleted[], int tableSize)
+{
+    for(int i = 0; i < tableSize; i++)
+        deleted[i] = 0;
+}
+
+int openInsertion(int * table[], int deleted[], int tableSize, int number)
+{
+    int key = hash(0, number, tableSize);
+    int start = key;
+    int deletedSlotKey = -1;
+
+    if (table[key] == NULL)
+    {
+        table[key] = malloc(sizeof(int));
+        *table[key] = number;
+        printf("%d -> %d\nOK\n", key, *table[key]);
+        return 0;
+    }
+
+    while (table[key] != NULL)
+    {
+        if (deleted[key] == 1 && deletedSlotKey == -1)
+            deletedSlotKey = key;
+        
+        if (*table[key] == number && deleted[key] == 0)
+        {
+            printf("%d EXISTS\n", number);
+            if (deletedSlotKey != -1)
+            {
+                *table[deletedSlotKey] = number;
+                deleted[deletedSlotKey] = 0;
+                deleted[key] = 1;
+            }
+            return 0;
+        }
+
+        key++;
+        if (key == tableSize)
+            key = 0;
+
+        if (key == start)
+        {
+            if (deletedSlotKey != -1)
+                break;
+            else
+            {
+            printf("GIVING UP!\n");
+            return 1;
+            }
+        }
+    }
+
+    if (deletedSlotKey != -1)
+    {
+        *table[deletedSlotKey] = number;
+        deleted[deletedSlotKey] = 0;
+        printf("%d -> %d\nOK\n", deletedSlotKey, number);
+    }
+    else
+    {
+        table[key] = malloc(sizeof(int));
+        *table[key] = number;
+        printf("%d -> %d\nOK\n", key, number);
+    }
+
+    return 0;
+}
+
+void openSearch(int * table[], int deleted[], int tableSize, int number)
+{
+    int key = hash(0, number, tableSize);
+    int start = key;
+    int deletedSlotKey = -1;
+
+    while (table[key] != NULL)
+    {
+        if (deleted[key] == 1 && deletedSlotKey == -1)
+            deletedSlotKey = key;
+
+        if (*table[key] == number && deleted[key] == 0)
+        {
+            if (deletedSlotKey != -1)
+            {
+                *table[deletedSlotKey] = number;
+                deleted[deletedSlotKey] = 0;
+                deleted[key] = 1;
+                printf("%d\n", deletedSlotKey);
+            }
+            else
+                printf("%d\n", key);
+            return;
+        }
+
+        key++;
+        if (key == tableSize)
+            key = 0;
+
+        if (key == start)
+            break;
+    }
+
+    printf("NO\n");
+}
+
+void openDelete(int * table[], int deleted[], int tableSize, int key, int number)
+{
+    int start = key;
+    while (table[key] != NULL)
+    {
+        if (*table[key] == number && deleted[key] == 0)
+        {
+            deleted[key] = 1;
+            printf("OK\n");
+            return;
+        }
+        key++;
+        if (key == tableSize)
+            key = 0;
+        if (key == start)
+        {
+            printf("NO\n");
+            return;
+        }
+    }
+}
+
+void openPrint(int * table[], int deleted[], int tableSize)
+{
+    for(int i = 0; i < tableSize; i++)
+    {
+        if (table[i] != NULL && deleted[i] == 0)
+        printf("%d\t%d\n", i, *table[i]);
+        else if (table[i] != NULL && deleted[i] == 1)
+        printf("%d\t%c\n", i, 'D');
+    }
+}
+
+void open(int tableSize)
+{
+    int * table[tableSize];
+    int deleted[tableSize];
+    initTable(table, tableSize);
+    initDeleted(deleted, tableSize);
+
+    char instruction[] = "A";
+    int number = -1;
+
+    while(scanf("%s", instruction) != EOF && instruction[0] != 'S')
+    {
+        int key;
+        if (instruction[0] != 'P')
+            assert(scanf("%d", &number) == 1);
+            
+        key = hash(0, number, tableSize);
+
+        if (instruction[0] == 'I')
+            if (openInsertion(table, deleted, tableSize, number))
+                break;
+
+        if (instruction[0] == 'C')
+            openSearch(table, deleted, tableSize, number);
+
+        if (instruction[0] == 'D')
+        {
+            if (table[key] == NULL)
+                printf("NO\n");
+            else if (*table[key] == number && deleted[key] == 0)
+            {
+                deleted[key] = 1;
+                printf("OK\n");
+            }
+            else
+                openDelete(table, deleted, tableSize, key, number);
+        }
+
+        if (instruction[0] == 'P')
+            openPrint(table, deleted, tableSize);
+    }
+}
+
+// // // // // // // // // // // // // //
+
+typedef struct linkedLists
+{
+    int key;
+    int nCount;
+    int numbers[2000];
+    struct linkedLists * prox;
+} *linkL;
+
+linkL linkInsert(linkL l, int tableSize, int number)
+{
+    int key = hash(0, number, tableSize);
+
+    int keyExists = 0;
+    int numberExists = 0;
+
+    // Caso a lista ligada ainda não exista
+    if (l == NULL)
+    {
+        l = malloc(sizeof(struct linkedLists));
+        l->key = key;
+        l->nCount = 1;
+        l->numbers[0] = number;
+        l->prox = NULL;
+        printf("%d -> %d\nOK\n", key, number);
+        return l;
+    }
+
+    linkL temp = l;
+
+    // Determina se existe já na lista a chave e o número pretendidos
+    while (temp != NULL && temp->key != key)
+        temp = temp->prox;
+
+    if (temp != NULL)
+    {
+        keyExists = 1;
+        for (int i = 0; i < temp->nCount; i++)
+            if ((temp->numbers)[i] == number)
+                numberExists = 1;
+    }
+
+    // Caso ambos existam print EXISTS
+    if (keyExists && numberExists)
+        printf("%d EXISTS\n", number);
+    
+    // Caso exista a chave mas não exista o número
+    else if (keyExists && !numberExists)
+    {
+        int numbers[temp->nCount+1];
+        numbers[0] = number;
+
+        for (int i = 0; i < temp->nCount; i++)
+            numbers[i+1] = temp->numbers[i];
+        for (int i = 0; i < temp->nCount+1; i++)
+            temp->numbers[i] = numbers[i];
+        
+        temp->nCount++;
+
+        printf("%d -> %d\nOK\n", key, number);
+    }
+
+    else
+    {
+        linkL r = malloc(sizeof(struct linkedLists));
+
+        r->key = key;
+        r->nCount = 1;
+        r->numbers[0] = number;
+        r->prox = NULL;
+        linkL temp = l;
+        linkL ant = NULL;
+        while(temp != NULL && temp->key < key)
+        {
+            ant = l;
+            temp = temp->prox;
+        }
+        if (ant == NULL)
+        {
+            r->prox = l;
+            l = r;
+        }
+        else
+        {
+            ant->prox = r;
+            r->prox = temp;
+        }
+        printf("%d -> %d\nOK\n", key, number);
+    }
+
+    return l;
+
+}
+
+void linkSearch(linkL l, int tableSize, int number)
+{
+int key = hash(0, number, tableSize);
+
+    int numberExists = 0;
+
+    if (l == NULL)
+        printf("NO\n");
+
+    linkL temp = l;
+
+    while (temp != NULL && temp->key != key)
+        temp = temp->prox;
+
+    if (temp != NULL)
+    {
+        for (int i = 0; i < temp->nCount; i++)
+            if ((temp->numbers)[i] == number)
+                numberExists = 1;
+    }
+
+    if(numberExists)
+        printf("%d\n", key);
+    else
+        printf("NO\n");
+}
+
+linkL linkDelete(linkL l, int tableSize, int number)
+{
+    int key = hash(0, number, tableSize);
+
+    int numberExists = 0;
+
+    if (l == NULL)
+        printf("NO\n");
+
+    linkL temp = l;
+    linkL ant = NULL;
+
+    while (temp != NULL && temp->key != key)
+    {
+        ant = temp;
+        temp = temp->prox;
+    }
+
+    if (temp != NULL)
+    {
+        for (int i = 0; i < temp->nCount; i++)
+            if ((temp->numbers)[i] == number)
+                numberExists = 1;
+    }
+
+    if (!numberExists)
+        printf("NO\n");
+    else
+    {
+        int newNumbers[temp->nCount];
+        int j = 0;
+
+        for (int i = 0; i < temp->nCount; i++)
+        {
+            if (temp->numbers[i] != number)
+            {
+                newNumbers[j] = temp->numbers[i];
+                j++;
+            }
+        }
+
+        temp->nCount--;
+
+        for (int i = 0; i < temp->nCount; i++)
+            temp->numbers[i] = newNumbers[i];
+
+        printf("OK\n");
+    }
+    
+    if (temp->nCount == 0)
+    {
+        if (ant == NULL)
+            return temp->prox;
+        else
+            ant->prox = temp->prox;
+    }
+
+
+
+    return l;
+}
+
+void linkPrint(linkL l)
+{
+    linkL temp = l;
+    while(temp != NULL)
+    {
+        printf("%d", temp->key);
+        for(int i = 0; i < temp->nCount; i++)
+            printf(" %d",temp->numbers[i]);
+        printf("\n");
+        temp = temp->prox;
+    }
+}
+
+void link(int tableSize)
+{
+    linkL l = NULL;
+
+    char instruction[] = "A";
+    int number = -1;
+
+    while(scanf("%s", instruction) != EOF && instruction[0] != 'S')
+    {
+        if (instruction[0] != 'P')
+            assert(scanf("%d", &number) == 1);
+
+        if (instruction[0] == 'I')
+            l = linkInsert(l, tableSize, number);
+
+        if (instruction[0] == 'C')
+        {
+            linkSearch(l, tableSize, number);
+        }
+
+        if (instruction[0] == 'D')
+        {
+            l = linkDelete(l, tableSize, number);
+        }
+
+        if (instruction[0] == 'P')
+        {
+            linkPrint(l);
+        }
+    }
+}
+
+
+void cuckooInsertion(int tableSize, int * tables[][tableSize], int currentTable, int number, int tries)
+{
+    int key = hash(currentTable, number, tableSize);
+    int kickedNumber = -1;
+    
+    if (tries > tableSize)
+    {
+        printf("GIVING UP!\n");
+        return;
+    }
+    else if (tables[currentTable][key] == NULL)
+    {
+        tables[currentTable][key] = malloc(sizeof(int));
+        *tables[currentTable][key] = number;
+        printf("%d %d -> %d\nOK\n", currentTable, key, *tables[currentTable][key]);
+    }
+    else
+    {
+        kickedNumber = *tables[currentTable][key];
+        *tables[currentTable][key] = number;
+        printf("%d %d -> %d\n", currentTable, key, *tables[currentTable][key]);
+
+        tries++;
+
+        currentTable = !currentTable;
+
+        cuckooInsertion(tableSize, tables, currentTable, kickedNumber, tries);
+    }
+}
+
+void cuckooPrint(int tableSize, int * tables[][tableSize])
+{
+    for(int i = 0; i < tableSize; i++)
+    {
+        if (tables[0][i] != NULL)
+        printf("%d\t%d\t%d\n", 0, i, *tables[0][i]);
+    }
+    for(int i = 0; i < tableSize; i++)
+    {
+        if (tables[1][i] != NULL)
+        printf("%d\t%d\t%d\n", 1, i, *tables[1][i]);
+    }
+}
+
+void cuckoo(int tableSize)
+{
+    int * tables[2][tableSize];
+    initTable(tables[0], tableSize);
+    initTable(tables[1], tableSize);
+
+    char instruction[] = "A";
+    int number = -1;
+
+    while(scanf("%s", instruction) != EOF && instruction[0] != 'S')
+    {
+        int key;
+        if (instruction[0] != 'P')
+            assert(scanf("%d", &number) == 1);
+            
+        key = hash(0, number, tableSize);
+
+        if (instruction[0] == 'I')
+        {
+            if (tables[0][key] != NULL && *tables[0][key] == number)
+                printf("%d EXISTS\n", *tables[0][key]);
+            else
+                cuckooInsertion(tableSize, tables, 0, number, 0);
+        }
+
+        if (instruction[0] == 'P')
+            cuckooPrint(tableSize, tables);
+    }
+}
+
+
+int main()
+{    
+    int tableSize;
+    assert(scanf("%d", &tableSize) == 1);
+
+    char protocol[7];
+    assert(scanf("%s", protocol) != 0);
+
+    if (!strcmp(protocol, "OPEN"))
+        open(tableSize);
+    if (!strcmp(protocol, "LINK"))
+        link(tableSize);
+    if (!strcmp(protocol, "CUCKOO"))
+        cuckoo(tableSize);
+    
+    return 0;
+}
